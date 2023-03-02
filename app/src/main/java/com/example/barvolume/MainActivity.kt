3e159fba8af2c2b2b3881574c9c1837aa5f2400a
ricky-kiva <ricky.kiva@gmail.com>
2023-03-02 17:46:15 +0700

    add calculate function

--- app/src/main/java/com/example/barvolume/MainActivity.kt
+++ app/src/main/java/com/example/barvolume/MainActivity.kt
@@ -2,11 +2,46 @@ package com.example.barvolume
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    // initiating variables that
+    private lateinit var edtWidth: EditText
+    private lateinit var edtHeight: EditText
+    private lateinit var edtLength: EditText
+    private lateinit var btnResult: Button
+    private lateinit var tvResult: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // assigning value to variables
+        // will find the 'view' object by 'id'
+        edtWidth = findViewById(R.id.edt_width)
+        edtHeight = findViewById(R.id.edt_height)
+        edtLength = findViewById(R.id.edt_length)
+        btnResult = findViewById(R.id.btn_result)
+        tvResult = findViewById(R.id.tv_result)
+
+        // this need to contract Interface 'View.OnClickListener'
+        btnResult.setOnClickListener(this)
+
+    }
+
+    // overriding from 'View.OnClickListener'
+    override fun onClick(v: View?) {
+        if (v?.id == R.id.btn_result) {
+            val inputLength = edtLength.text.toString().trim()
+            val inputWidth = edtWidth.text.toString().trim()
+            val inputHeight = edtHeight.text.toString().trim()
+
+            val volume = inputLength.toDouble() * inputWidth.toDouble() * inputHeight.toDouble()
+            tvResult.text = volume.toInt().toString()
+        }
     }
 }
