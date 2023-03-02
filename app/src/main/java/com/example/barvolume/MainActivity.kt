ed5779435136a8ec8c551ab3cefd4b348ffc24f0
ricky-kiva <ricky.kiva@gmail.com>
2023-03-02 18:33:16 +0700

    add comments

--- app/src/main/java/com/example/barvolume/MainActivity.kt
+++ app/src/main/java/com/example/barvolume/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.barvolume
 
+// kotlin has 'code completion' that 'auto import'
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -7,29 +8,49 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 
+// appCompatActivity() is the superclass for 'activity'-es
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     // initiating variables that
+    // 'lateinit' means the variable will be initialized later
     private lateinit var edtWidth: EditText
     private lateinit var edtHeight: EditText
     private lateinit var edtLength: EditText
     private lateinit var btnResult: Button
     private lateinit var tvResult: TextView
 
+    // this is the 'main method' to create an 'activity' where an activity is 'created'
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // 'activity layout' is from 'activity_main.xml'
         setContentView(R.layout.activity_main)
 
         // assigning value to variables
-        // will find the 'view' object by 'id'
+        // 'id' is created on 'R' file 'deep inside' 'app' folder
+        // will find the 'view' (EditText) object by 'id'
         edtWidth = findViewById(R.id.edt_width)
         edtHeight = findViewById(R.id.edt_height)
         edtLength = findViewById(R.id.edt_length)
         btnResult = findViewById(R.id.btn_result)
         tvResult = findViewById(R.id.tv_result)
 
-        // this need to contract Interface 'View.OnClickListener'
+        // need to contract for Interface 'View.OnClickListener'
+        // 'this' referers the current 'activity' object
         btnResult.setOnClickListener(this)
+
+        /* It could also be written 'without adding Interface' like:
+
+        btnResult.setOnClickListener(View.OnClickListener {
+           // the action when the button is clicked
+        })
+
+        OR SIMPLY
+
+        btnResult.setOnClickListener {
+           // the action when the button is clicked
+        }
+
+        */
 
         // calling the 'saved state' if there is any
         if (savedInstanceState != null) {
@@ -69,6 +90,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     // to enable 'saving state' when 'rotated'
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
+        // 'putString()' has concept of 'key-value'
         outState.putString(STATE_RESULT, tvResult.text.toString())
     }
 }
